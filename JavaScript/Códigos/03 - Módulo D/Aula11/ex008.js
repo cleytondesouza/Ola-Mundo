cbe9ae1e63bd189fa6a7bb296bef11fd23110a71
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2021-03-10 15:45:38 -0300

    Primeira Aula do Capitulo 11

---
+++ JavaScript/Códigos/03 - Módulo D/Aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 36
+    console.log(`A velocidade do carro é ${velocidade} km por hora`)
+if(velocidade > 50){
+    console.log(`Você foi multado`) //Condição simples
+}
+    console.log(`Você está dentro da velocidade permitida`)
+
---
+++ JavaScript/Códigos/03 - Módulo D/Aula11/ex009.js
@@ -0,0 +1,11 @@
+//Condição composta
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+
+if(pais == 'Brasil'){ 
+    console.log('Você é Brasileiro')
+    
+}else{
+    console.log('Você é Estrangeiro')
+}

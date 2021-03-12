44029a687d6d03a82d83df406a4cf97820e261c2
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2021-03-12 12:33:27 -0300

    Update script.js

--- JavaScript/Códigos/03 - Módulo D/Aula12/exe1/script.js
+++ JavaScript/Códigos/03 - Módulo D/Aula12/exe1/script.js
@@ -2,8 +2,8 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora  = 10
+    var hora = data.getHours()
+    //var hora  = 10
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 5 && hora < 12){
         img.src = 'img/manha.png'

2f78bab4771ecd3b702f946bc457d32aac5ad40d
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2021-03-12 12:24:06 -0300

    Parte 2 JS Bom dia, Boa tarde e Boa noite

--- JavaScript/Códigos/03 - Módulo D/Aula12/exe1/script.js
+++ JavaScript/Códigos/03 - Módulo D/Aula12/exe1/script.js
@@ -1,1 +1,19 @@
-window.alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora  = 19
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'img/manha.png'
+        msg2.innerHTML = 'Bom dia'   
+    }else if(hora >= 12 && hora < 18){
+        img.src = 'img/tarde.png'
+        msg2.innerHTML = 'Boa tarde'
+    }else{
+        
+        img.src = 'img/noite.png'
+        msg2.innerHTML = 'Boa Noite'
+    }
+}

dc4529cf0b389a9899a45558a131f0180d6fbe0a
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2021-03-12 22:34:59 -0300

    Hora automática

--- JavaScript/Códigos/03 - Módulo D/Aula12/exe1/script.js
+++ JavaScript/Códigos/03 - Módulo D/Aula12/exe1/script.js
@@ -3,7 +3,7 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    var hora  = 13
+    //var hora  = 13
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 5 && hora < 12){
         img.src = 'img/manha.png'

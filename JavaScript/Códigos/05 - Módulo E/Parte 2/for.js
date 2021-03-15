25772fe1e5281c8b0337fc0d2755f9d36930eac9
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2021-03-15 10:25:55 -0300

    Aula de for completa

---
+++ JavaScript/Códigos/05 - Módulo E/Parte 2/for.js
@@ -0,0 +1,12 @@
+//Estrutura de repetição for (PARA)
+/*
+Essa estrutura tem 3 passos
+1 - Inicio - criando a variavel e atribuido valor
+2 - Teste lógico
+3 - Incremento
+
+*/
+
+for(var c = 1; c <= 10; c++){
+    console.log(`Número ${c}`)
+}

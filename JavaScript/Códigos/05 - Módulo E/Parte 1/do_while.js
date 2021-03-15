0239f0c7621c683deac261dbdf8e61f74c476701
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2021-03-15 10:12:47 -0300

    Estruturas de repetição

---
+++ JavaScript/Códigos/05 - Módulo E/Parte 1/do_while.js
@@ -0,0 +1,7 @@
+//Estrtura de repetição com teste lógico no final
+//do significa faça e while significa enquanto
+var c = 1
+do{
+    console.log(`Número ${c}`)
+    c++
+}while(c <= 10)
---
+++ JavaScript/Códigos/05 - Módulo E/Parte 1/while.js
@@ -0,0 +1,19 @@
+//Estrutura de repetição com teste lógico no início
+
+var contador = 1
+while(contador <= 10){
+    console.log(`${contador}ª Fatia`)
+    contador++
+}
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/

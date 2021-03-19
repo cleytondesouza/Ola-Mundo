94e3c890b9e8e6e38c3da147ef5d0fc151b851de
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2021-03-19 08:37:27 -0300

    Exercício Final - Finalizado

--- JavaScript/Códigos/06 - Módulo F/exercicio/script.js
+++ JavaScript/Códigos/06 - Módulo F/exercicio/script.js
@@ -21,9 +21,42 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        window.alert('Tudo OK')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor Inválido ou já encontrado na Lista')
     }
+    num.value = ''
+    num.focus()
+}
 
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Não existem valore!')
+    } else {
+        let tot = valores.length
+        let menor = valores[0]
+        let maior = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados</P>`
+        res.innerHTML += `<p>O MENOR valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>O MAIOR valor informado foim ${maior}</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
+        res.innerHTML += `<p>A média entre os valores é ${media}</p>`
+    }
 }
